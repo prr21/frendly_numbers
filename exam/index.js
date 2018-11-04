e5aa05dbe81956965ace0ffa4b78bf3c52d94273
Kurban <mr.gustav009@gmail.com>
2018-11-04 18:22:50 +0300

    Функция упрощена

--- exam/index.js
+++ exam/index.js
@@ -1,43 +1,39 @@
+'use strict'
 function getFriendlyNumbers(start, end) {
+	var numbersArr = {},
+		result = [];
+
 	if (start <= end && typeof(start || end) === 'number' && (start || end) > 0) {
-		let result = [],
-		numbersArr = [];
 	
-	    for (let number = start; number <= end; number++) {
+	    for (let startNum = start; startNum <= end; startNum++) {
 			let numbersSum = 0;
 	
-		    for (let i = 1; i < number; i++) {
-				let divider = number / i;
-		    	if ((divider ^ 0) === divider) {	    	
+		    for (let i = 1; i < startNum; i++) {
+				let divider = startNum / i;
+
+		    	if ((divider ^ 0) === divider) {
 		    		numbersSum += i;
 		    	}
 		    };
 		    
-		    if(numbersSum < start) continue;
-	
-		    function Number(value, sum) {
-				this.value = value;
-				this.sum = sum;
-			};
-	
-	    	let numObj = new Number(number, numbersSum);
-	    	numbersArr.push(numObj)
+		    if (numbersSum < start) continue;
+		    else numbersArr[startNum] = numbersSum;
 		};
-	
-		for (let i = 0; i < numbersArr.length; i++) {
-			for (let j = 0; j < numbersArr.length; j++) {
-				let resultNum = [];
-	
-				if (numbersArr[i].value === numbersArr[j].sum && numbersArr[j].value === numbersArr[i].sum && numbersArr[i].value !== numbersArr[i].sum && numbersArr[i].value < numbersArr[i].sum) {
-					resultNum.push(numbersArr[i].value);
-					resultNum.push(numbersArr[j].value);
-					result.push(resultNum);
-					break;
-				}
-			}		
-		} return result;
-	} else return false;
-}
+	};
+
+	for (let i in numbersArr) {
+		for (let j in numbersArr) {
+			let resultNum = [];
+
+			if (i == numbersArr[j] && j == numbersArr[i] && i < numbersArr[i]) {
+				resultNum.push(+i);
+				resultNum.push(+j);
+				result.push(resultNum);
+			}
+		}
+	}
+	return result;
+}	
 
 console.log(getFriendlyNumbers(200, 1220));
 
